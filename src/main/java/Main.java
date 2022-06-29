d3334d2d603af12b492755294d6582eae2b08708
DiankaFernando <diankafernando@gmail.com>
2022-06-29 21:58:01 +0200

    directories

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,7 +26,6 @@ public class Main extends Application {
 		primaryStage.setMinHeight(350);
 		primaryStage.setWidth(450);
 		primaryStage.setHeight(350);
-		System.out.println("hasjkhdsjkd");
 	}
 
 	public static void main(String[] args) {
